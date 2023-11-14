120b913a874b82519686b9d30a7dad75c466178c
Diego Amil González <diegoamilglz@gmail.com>
2023-11-14 19:04:34 +0100

    prueba comitear visual code

--- SushiGo/src/App.java
+++ SushiGo/src/App.java
@@ -2,6 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World! Este mensaje ha sido editado en mi PC");
         int a = 3;
-        b=random(a);
+        int b=a*3;
+        System.err.println(b); 
     }
 }
