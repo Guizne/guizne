1d76b654d3c66b361eec3c74a424c538d5f633a7
Diego Amil González <diegoamilglz@gmail.com>
2023-11-14 18:54:19 +0100

    Update App.java

--- SushiGo/src/App.java
+++ SushiGo/src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World! Este mensaje ha sido editado en mi PC");
+        int a = 3;
+        b=random(a);
     }
 }
